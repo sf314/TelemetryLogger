1cb7e2bad2f7ffb3c55263ac485fe65a3995f577
smflore4 <smflore4@asu.edu>
2017-06-04 09:48:52 -0700

    Initial Commit

---
+++ TelemetryLogger/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TelemetryLogger
+//
+//  Created by Stephen Flores on 6/4/17.
+//  Copyright © 2017 Stephen Flores. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
